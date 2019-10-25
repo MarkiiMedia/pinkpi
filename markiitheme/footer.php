0675f864e55148b5831ebfa11825f3cc7ec311e9
Mark Andersen <31654229+MarkiiMedia@users.noreply.github.com>
2019-10-25 13:11:54 +0200

    theme
    markiitheme theme folder

---
+++ markiitheme/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package MarkiiTheme
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<a class="footer_text"> PinkPi - MarkiiMedia
+			</a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
